6a744b0f0667340eb93b60e5e8c793fe7912b436
Jack Biddlecombe <jackbidders@gmail.com>
2015-09-30 20:55:00 +0100

    changed how generating weighted random icon works and started win conditions

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
 		orange: "<img src='assets/images/icon_orange.png' alt='orange' height='150' width='150'>" 
 	}
 
+	var slots = [ {html: $("#slot-1"), hasSpun: false}, {html: $("#slot-2"), hasSpun: false}, {html: $("#slot-3"), hasSpun: false}];
+
+	var isSpinning = false;
+
 	function pickRandomProperty(obj) {
     	var result;
     	var count = 0;
@@ -21,10 +25,25 @@ $(document).ready(function(){
     	return result;
 	}
 
+	function pickSlot(fruits){
+		var total = 0;
+    	var randNumber = Math.round(Math.random()*100);
+    		
+    	for(var i=0; i<fruits.length;i++){  		
+    		if(randNumber >= total && randNumber <= total+fruits[i][1])
+    			return icons[fruits[i][0]];
+    		total+=fruits[i][1];
+    	}
+	}
+
+	function changeSlot(slot,value){
+		slot.html(value);
+	}
+
 	function changeSlots(slot1, slot2, slot3){
-		$("#slot-1").html(slot1);
-		$("#slot-2").html(slot2);
-		$("#slot-3").html(slot3);
+		slots[0].html.html(slot1);
+		slots[1].html.html(slot2);
+		slots[2].html.html(slot3);
 	}
 
 	function allRandomSlots(){
@@ -32,24 +51,58 @@ $(document).ready(function(){
 		changeSlots(icons[arr[0]], icons[arr[1]], icons[arr[2]]);
 	}
 
-	
-	changeSlots(icons.big_win, icons.big_win, icons.big_win);
-
 	function spin(){
+		isSpinning = true;
 		var counter = 0;
 		function timer(){
-			allRandomSlots();
+			// Keep flicking through slots changing them
+			for(var i=0; i<slots.length; i++){
+				if(!slots[i].hasSpun){
+					changeSlot(slots[i].html, icons[pickRandomProperty(icons)]);
+				}
+			}
 			counter++;
-			if(counter >= 15){
+
+			// Change slots at different times
+			if(counter === 40){
+				slots[2].hasSpun = true;
+				changeSlot(slots[2].html, pickSlot([["plum", 25], ["lemon", 20], ["cherries", 10], ["bananas", 10], ["orange", 10], ["melon", 10], ["bar", 7], ["seven", 5], ["big_win", 3]]));
 				clearInterval(interval);
+				isSpinning = false;
+				checkWin();
+			}else if(counter === 30){
+				slots[1].hasSpun = true;
+				changeSlot(slots[1].html, pickSlot([["plum", 25], ["lemon", 20], ["cherries", 10], ["bananas", 10], ["orange", 10], ["melon", 10], ["bar", 7], ["seven", 5], ["big_win", 3]]));
+			}else if(counter === 20){
+				slots[0].hasSpun = true;
+				changeSlot(slots[0].html, pickSlot([["plum", 25], ["lemon", 20], ["cherries", 10], ["bananas", 10], ["orange", 10], ["melon", 10], ["bar", 7], ["seven", 5], ["big_win", 3]]));
 			}
 		}
-		var interval = setInterval(timer, 100);
+		var interval = setInterval(timer, 75);
+		for(var slot in slots)
+			slots[slot].hasSpun = false;
 	}
 
+	function checkWin(){
+		var first = slots[0].html.contents().attr("alt");
+		var second = slots[1].html.contents().attr("alt");
+		var third = slots[2].html.contents().attr("alt");
+
+		if(first === second && second === third){
+			console.log("3 matches!");
+		}else if(first === second || second === third || first === third){
+			console.log("2 matches");
+		}else{
+			console.log("no matches");
+		}
+	}
+
+	changeSlots(icons.big_win, icons.big_win, icons.big_win);
 
 	$("button").click(function(){
-		spin();
+		if(!isSpinning){
+			spin();
+		}
 	});
 
 });
